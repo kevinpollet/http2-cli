94695c7d7ba8d0f28adb2925b671e887a97a5714
kevinpollet <pollet.kevin@gmail.com>
2019-05-06 14:09:33 +0400

    Add yargs dependency

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,31 @@
  * found in the LICENSE.md file.
  */
 
-process.stdout.write("Hello World");
+import yargs from "yargs";
+import http2 from "http2";
+import { URL } from "url";
+
+const { method, url } = yargs
+  .scriptName("http2")
+  .showHelpOnFail(true)
+  .command("$0 <method> <url>", "default command", yargs =>
+    yargs
+      .positional("method", {
+        choices: ["get"],
+        description: "The HTTP method",
+        type: "string",
+      })
+      .positional("url", {
+        type: "string",
+      })
+  )
+  .help()
+  .version().argv;
+
+const { origin, pathname } = new URL(url!);
+const client = http2.connect(origin);
+
+client
+  .request({ ":method": method!.toUpperCase(), ":path": pathname })
+  .on("response", headers => JSON.stringify(headers))
+  .on("data", data => process.stdout.write(data));
---
+++ src/types/yargs.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file.
+ */
+
+import { Arguments } from "yargs";
+
+declare module "yargs" {
+  interface Argv<T = {}> {
+    command<U>(
+      command: string | ReadonlyArray<string>,
+      description: string,
+      builder?: (args: Argv<T>) => Argv<U>,
+      handler?: (args: Arguments<U>) => void
+    ): Argv<U>;
+  }
+}
