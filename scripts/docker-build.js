21cdae3beaa665388e28fa9bcb69657bcab73efd
kevinpollet <pollet.kevin@gmail.com>
2019-05-31 14:50:39 +0400

    Add scripts to build and publish docker images

---
+++ scripts/docker-build.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const execa = require("execa");
+const { join } = require("path");
+const { parse } = require("semver");
+
+const dockerId = "kevinpollet";
+const { version, major, minor } = parse(process.env.npm_package_version);
+const dockerTags = ["latest", `${major}`, `${major}.${minor}`, `${version}`];
+
+const subprocess = execa("docker", [
+  "build",
+  join(__dirname, ".."),
+  ...[].concat(
+    ...dockerTags.map(tagName => [
+      "-t",
+      `${dockerId}/${process.env.npm_package_name}:${tagName}`,
+    ])
+  ),
+]);
+
+subprocess.stdout.pipe(process.stdout);
+
+subprocess.catch(err => {
+  process.stderr.write(`${err.message}\n`);
+  process.exit(err.code);
+});
---
+++ scripts/docker-publish.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const execa = require("execa");
+const { parse } = require("semver");
+
+const dockerId = "kevinpollet";
+const { version, major, minor } = parse(process.env.npm_package_version);
+const dockerTags = ["latest", `${major}`, `${major}.${minor}`, `${version}`];
+
+const subprocesses = dockerTags.map(tagName =>
+  execa("docker", [
+    "push",
+    `${dockerId}/${process.env.npm_package_name}:${tagName}`,
+  ])
+);
+
+Promise.all(subprocesses).catch(err => {
+  process.stderr.write(`${err.message}\n`);
+  process.exit(err.code);
+});
